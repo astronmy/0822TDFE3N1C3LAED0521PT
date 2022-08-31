1ce7731929019c30922a5b85decd7f0f19e00760
Renzo Vinci <renzvinci@gmail.com>
2022-08-31 17:48:02 -0300

    before class

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
 import './App.css';
-import { getPokemones } from "./data/pokemones";
-import Container from './components/Container';
-import CustomButton from './components/CustomButton';
-import { useState } from 'react';
 
 function App() {
-  const [contador, setContador] = useState(0)
-  
-  const incrementar = () => {
-      console.log('se ejecuto la funcion de incrementar')
-      setContador(contador+1)
-  }
  
   return (
       <>
-        <h2>{contador}</h2>
-        <CustomButton funcion={incrementar}/>
       </>
   );
 }
